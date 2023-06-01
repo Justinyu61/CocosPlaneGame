2711d8cf1bd47983417547116cb88ec46652f022
Justinyu <justinyu61@gmail.com>
2023-06-01 16:40:19 +0800

    調整飛行控制邏輯

--- assets/Script/AirplaneOneController .ts
+++ assets/Script/AirplaneOneController .ts
@@ -1,5 +1,10 @@
 const { ccclass, property } = cc._decorator;
 
+const MAX_X = 50;
+const MIN_X = -50;
+const MAX_Y = 150;
+const MIN_Y = -10;
+
 @ccclass
 export default class AirplaneOneController extends cc.Component {
   @property({ displayName: "播放速度" })
@@ -7,7 +12,9 @@ export default class AirplaneOneController extends cc.Component {
   @property({ displayName: "圖片", type: [cc.SpriteFrame] })
   spriteFrames: cc.SpriteFrame[] = [];
   @property({ displayName: "飛行速度"})
-  flySpeed: number = 100;
+  flySpeed: number = 0;
+  @property({ displayName: "初始位置"})
+  initialPosition: cc.Vec3 = cc.Vec3.ZERO;
 
   private isMovingUP: boolean = false;
   private isMovingDown: boolean = false;
@@ -15,6 +22,7 @@ export default class AirplaneOneController extends cc.Component {
   private isMovingLeft: boolean = false;
 
   start() {
+    this.node.position = this.initialPosition;
     const frames: cc.SpriteFrame[] = [];
     
     for (let i = 0; i < this.spriteFrames.length; i++) {
@@ -32,73 +40,65 @@ export default class AirplaneOneController extends cc.Component {
     }
   }
 
-  onKeyDown(event: cc.Event.EventKeyboard){
-    console.warn(event.keyCode);
-    
+  handleKeyCode(event: cc.Event.EventKeyboard, isTap: boolean){
     switch (event.keyCode) {
       case cc.macro.KEY.w:
-        this.isMovingUP = true;
-        console.warn(this.isMovingUP);
+      case cc.macro.KEY.up:
+        this.isMovingUP = isTap;
         break;
       case cc.macro.KEY.s:
-        this.isMovingDown = true;
-        console.warn(this.isMovingDown);
+      case cc.macro.KEY.down:
+        this.isMovingDown = isTap;
         break;
       case cc.macro.KEY.d:
-        this.isMovingRight = true;
-        console.warn(this.isMovingRight);
+      case cc.macro.KEY.right:
+        this.isMovingRight = isTap;
         break;
       case cc.macro.KEY.a:
-        this.isMovingLeft = true;
-        console.warn(this.isMovingLeft);
+      case cc.macro.KEY.left:
+        this.isMovingLeft = isTap;
         break;
     }
   }
 
+  onKeyDown(event: cc.Event.EventKeyboard){
+    this.handleKeyCode(event, true)
+  }
+
   onKeyUp(event: cc.Event.EventKeyboard){
-    switch (event.keyCode) {
-      case cc.macro.KEY.w:
-        this.isMovingUP = false;
-        console.warn(this.isMovingUP);
-        break;
-      case cc.macro.KEY.s:
-        this.isMovingDown = false;
-        console.warn(this.isMovingDown);
-        break;
-      case cc.macro.KEY.d:
-        this.isMovingRight = false;
-        console.warn(this.isMovingRight);
-        break;
-      case cc.macro.KEY.a:
-        this.isMovingLeft = false;
-        console.warn(this.isMovingLeft);
-        break;
-    }
+    this.handleKeyCode(event, false)
   }
 
   update(dt: number) {
     if (this.isMovingUP) {
-      this.node.y += this.flySpeed * dt;
+      if (this.node.y < MAX_Y) {
+        this.node.y += this.flySpeed * dt;
+      }
     }
     if (this.isMovingDown) {
-      this.node.y -= this.flySpeed * dt;
-      
+      if (this.node.y > MIN_Y) {
+        this.node.y -= this.flySpeed * dt;
+      }
     }
     if (this.isMovingRight) {
-      this.node.x += this.flySpeed * dt;
+      if (this.node.y < MAX_X) {
+        this.node.x += this.flySpeed * dt;
+      }
     }
     if (this.isMovingLeft) {
-      this.node.x -= this.flySpeed * dt;
+      if (this.node.y > MIN_X) {
+        this.node.x -= this.flySpeed * dt;
+      }
     }
   }
 
   onLoad(){
     cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-    cc.systemEvent
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
   }
 
-  // onDestroy(){
-  //   cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-  //   cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
-  // }
+  onDestroy(){
+    cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+  }
 }
